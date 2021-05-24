db6b1f19f11adafdbe6423d7b2b5d1cac59ed2d8
Jorge <jdieguezbean@gmail.com>
2021-05-24 20:37:28 +0300

    Get API URL from window.location.host

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,11 +3,11 @@ console.log(window.location.hostname);
 export const environment = {
   production: true,
   // @ts-ignore
-  SERVER_API_URL: 'http://' + (window.env.SERVER_HOST || '127.0.0.1') + ':8080/',
+  SERVER_API_URL: 'http://' + window.location.host + ':8080/',
   // @ts-ignore
-  GRAFANA_URL: 'http://' + (window.env.SERVER_HOST || '127.0.0.1') + ':3000/',
+  GRAFANA_URL: 'http://' + window.location.host + ':3000/',
   // @ts-ignore
-  PROMETHEUS_URL: 'http://' + (window.env.SERVER_HOST || '127.0.0.1') + ':9090/',
+  PROMETHEUS_URL: 'http://' + window.location.host + ':9090/',
   SESSION_AUTH_TOKEN: window.location.host.split(':')[0].toLocaleUpperCase(),
   BUILD_TIMESTAMP: new Date().getTime(),
   DEBUG_INFO_ENABLED: true,
